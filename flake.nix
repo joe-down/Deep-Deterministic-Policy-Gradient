b6ef6cf294bca73c18085f01f3494d09c4b3fefa
Joe <30624504+joe-down@users.noreply.github.com>
2024-10-14 21:32:16 +0100

    Setup

--- flake.nix
+++ flake.nix
@@ -10,14 +10,21 @@
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs;
-            [
-              (pkgs.python3.withPackages (python-pkgs: [
-                python-pkgs.pettingzoo
-                python-pkgs.pygame
-                python-pkgs.pymunk
-              ]))
-            ];
+          packages = with pkgs; [
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.gymnasium
+              python-pkgs.jax
+              python-pkgs.jaxlib
+              python-pkgs.matplotlib
+              python-pkgs.moviepy
+              python-pkgs.pybox2d
+              python-pkgs.pygame
+              python-pkgs.pytestCheckHook
+              python-pkgs.scipy
+            ]))
+            ffmpeg
+            opencv4
+          ];
         };
       });
     };
