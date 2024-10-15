ff01980fe32b50c6aee23ad4a27cd9cedf94e367
Joe <30624504+joe-down@users.noreply.github.com>
2024-10-15 13:14:03 +0100

    Setup

--- flake.nix
+++ flake.nix
@@ -5,22 +5,24 @@
       supportedSystems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
+        nixpkgs.lib.genAttrs supportedSystems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          });
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             (pkgs.python3.withPackages (python-pkgs: [
+              #Gymnasium
               python-pkgs.gymnasium
-              python-pkgs.jax
-              python-pkgs.jaxlib
-              python-pkgs.matplotlib
-              python-pkgs.moviepy
               python-pkgs.pybox2d
               python-pkgs.pygame
-              python-pkgs.pytestCheckHook
-              python-pkgs.scipy
+              #Pytorch
+              python-pkgs.torch-bin
             ]))
             ffmpeg
             opencv4
