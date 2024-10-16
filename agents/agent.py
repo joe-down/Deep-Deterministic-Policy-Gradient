7d56163c305ce354ebc022956a11aa094086e4d9
Joe <30624504+joe-down@users.noreply.github.com>
2024-10-16 17:51:39 +0100

    remove buffer code now in other file

--- agents/agent.py
+++ agents/agent.py
@@ -1,64 +1,6 @@
-import typing
-
 import numpy
 import torch
-
-
-class Buffer:
-    BUFFER_SIZE: int = 2 ** 22
-    assert BUFFER_SIZE > 0
-
-    def __init__(self, nn_input: int) -> None:
-        self.observations = torch.zeros((self.BUFFER_SIZE, nn_input))
-        self.observation_index = 0
-        self.rewards = torch.zeros(self.BUFFER_SIZE)
-        self.terminations = torch.zeros(self.BUFFER_SIZE)
-        self.terminations[0] = 1
-        self.reward_index = 0
-
-    def push_observation(self, observation: torch.tensor) -> None:
-        self.observation_index = (self.observation_index + 1) % self.BUFFER_SIZE
-        self.observations[self.observation_index] = observation
-
-    def push_reward(self, reward: float, terminated: bool) -> None:
-        self.reward_index = (self.reward_index + 1) % self.BUFFER_SIZE
-        self.rewards[self.reward_index] = reward
-        self.terminations[self.reward_index] = terminated
-
-    def random_episode(self) -> typing.Optional[tuple[torch.tensor, torch.tensor, torch.tensor]]:
-        episode_boundaries = self.terminations.nonzero() + 1
-        episode_count = len(episode_boundaries) - 1
-        if episode_count < 1:
-            return None
-        episode_number = torch.randint(0, episode_count, (1,))
-        episode_boundary = episode_boundaries[episode_number:episode_number + 2]
-        assert episode_boundary.shape == (2, 1)
-        episode_slice = slice(episode_boundary[0], episode_boundary[1])
-        return self.observations[episode_slice], self.rewards[episode_slice], self.terminations[episode_slice]
-
-    def random_observation(self) -> typing.Optional[tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor]]:
-        assert self.observation_index == self.reward_index
-        random_episode = self.random_episode()
-        if random_episode is None:
-            return None
-        observations, rewards, terminations = random_episode
-        assert len(observations) == len(rewards)
-        observation_index = torch.randint(0, len(observations), (1,))
-        return observations[observation_index], observations[(observation_index + 1) % len(observations)], rewards[
-            observation_index], terminations[observation_index]
-
-    def random_observations(self, number: int) \
-            -> typing.Optional[tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor]]:
-        observations = torch.zeros((number, 28))
-        next_observations = torch.zeros((number, 28))
-        rewards = torch.zeros((number, 1))
-        terminations = torch.zeros((number, 1))
-        for i in range(number):
-            random_observation = self.random_observation()
-            if random_observation is None:
-                return None
-            observations[i], next_observations[i], rewards[i], terminations[i] = random_observation
-        return observations, next_observations, rewards, terminations
+from buffer import Buffer
 
 
 class Agent:
