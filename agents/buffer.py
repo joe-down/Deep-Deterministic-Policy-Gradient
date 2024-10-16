a5e94fc4ad23fc89993cd4501d45871ed52c2c51
Joe <30624504+joe-down@users.noreply.github.com>
2024-10-16 18:12:15 +0100

    buffer fill logic fixes

--- agents/buffer.py
+++ agents/buffer.py
@@ -9,31 +9,38 @@ class Buffer:
     def __init__(self, nn_input: int) -> None:
         self.observations = torch.zeros((self.BUFFER_SIZE, nn_input))
         self.observation_index = 0
+
         self.rewards = torch.zeros(self.BUFFER_SIZE)
         self.terminations = torch.zeros(self.BUFFER_SIZE)
         self.terminations[0] = 1
-        self.reward_index = 0
-        self.buffer_fill: int = 0
+        self.reward_termination_index = 0
+
+        self.buffer_start: int = 1
+        self.buffer_end: int = 0
 
     def push_observation(self, observation: torch.tensor) -> None:
         self.observation_index = (self.observation_index + 1) % self.BUFFER_SIZE
         self.observations[self.observation_index] = observation
-        self.update_fill()
+        self.update_start_end()
 
     def push_reward(self, reward: float, terminated: bool) -> None:
-        self.reward_index = (self.reward_index + 1) % self.BUFFER_SIZE
-        self.rewards[self.reward_index] = reward
-        self.terminations[self.reward_index] = terminated
-        self.update_fill()
+        self.reward_termination_index = (self.reward_termination_index + 1) % self.BUFFER_SIZE
+        self.rewards[self.reward_termination_index] = reward
+        self.terminations[self.reward_termination_index] = terminated
+        self.update_start_end()
 
-    def update_fill(self) -> None:
-        self.buffer_fill = max(self.buffer_fill, min(self.observation_index, self.reward_index))
+    def update_start_end(self) -> None:
+        self.buffer_end = max(self.buffer_end, min(self.observation_index, self.reward_termination_index))
+        if self.observation_index == self.reward_termination_index == 0 and self.buffer_end == self.BUFFER_SIZE - 1:
+            self.buffer_start = 0
 
-    def random_observations(self, number: int) \
-            -> typing.Optional[tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor]]:
-        assert self.buffer_fill > 0
-        indexes = torch.randint(0, self.buffer_fill, (number,))
+    def buffer_observations_ready(self) -> bool:
+        return self.buffer_end >= self.buffer_start + 1
+
+    def random_observations(self, number: int) -> tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor]:
+        assert self.buffer_observations_ready()
+        indexes = torch.randint(self.buffer_start, self.buffer_end + 1, (number,))
         return (self.observations[indexes],
-                self.observations[indexes + 1],
+                self.observations[(indexes + 1) % self.BUFFER_SIZE],
                 self.rewards[indexes].unsqueeze(1),
                 self.terminations[indexes].unsqueeze(1))
