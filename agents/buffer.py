a4dfa6c6695d385dd238645752d2a4f1d7d43c82
Joe <30624504+joe-down@users.noreply.github.com>
2024-10-26 23:16:45 +0100

    buffer rework

--- agents/buffer.py
+++ agents/buffer.py
@@ -2,47 +2,52 @@ import torch
 
 
 class Buffer:
-    BUFFER_SIZE: int = 2 ** 28
-    assert BUFFER_SIZE > 0
-
     def __init__(self, nn_input: int) -> None:
-        self.observations = torch.zeros((self.BUFFER_SIZE, nn_input))
-        self.observation_index = 0
+        self.BUFFER_SIZE: int = 2 ** 3
+        assert self.BUFFER_SIZE > 0
 
-        self.rewards = torch.zeros(self.BUFFER_SIZE)
-        self.terminations = torch.zeros(self.BUFFER_SIZE)
-        self.terminations[0] = 1
-        self.reward_termination_index = 0
+        assert nn_input >= 1
+        self.nn_input_length: int = nn_input
 
-        self.buffer_start: int = 1
+        self.observations: torch.Tensor = torch.zeros((self.BUFFER_SIZE, self.nn_input_length))
+        self.observation_index: int = 0
+
+        self.rewards: torch.Tensor = torch.zeros(self.BUFFER_SIZE)
+        self.terminations: torch.Tensor = torch.zeros(self.BUFFER_SIZE)
+        self.reward_termination_index: int = 0
+
         self.buffer_end: int = 0
 
     def push_observation(self, observation: torch.tensor) -> None:
-        self.observation_index = (self.observation_index + 1) % self.BUFFER_SIZE
+        assert observation.shape == (self.nn_input_length,)
+        assert self.observation_index == self.reward_termination_index
         self.observations[self.observation_index] = observation
-        self.update_start_end()
+        self.observation_index = (self.observation_index + 1) % self.BUFFER_SIZE
 
     def push_reward(self, reward: float, terminated: bool) -> None:
-        self.reward_termination_index = (self.reward_termination_index + 1) % self.BUFFER_SIZE
+        assert self.reward_termination_index == (self.observation_index - 1) % self.BUFFER_SIZE
         self.rewards[self.reward_termination_index] = reward
         self.terminations[self.reward_termination_index] = terminated
-        self.update_start_end()
+        self.reward_termination_index = (self.reward_termination_index + 1) % self.BUFFER_SIZE
+        self.update_end()
 
-    def update_start_end(self) -> None:
-        self.buffer_end = max(self.buffer_end, min(self.observation_index, self.reward_termination_index))
-        if self.observation_index == self.reward_termination_index == 0 and self.buffer_end == self.BUFFER_SIZE - 1:
-            self.buffer_start = 0
+    def update_end(self) -> None:
+        assert self.observation_index == self.reward_termination_index
+        self.buffer_end = max(self.buffer_end, self.observation_index)
 
     def buffer_observations_ready(self) -> bool:
-        return self.buffer_end >= self.buffer_start + 1
+        return self.buffer_end >= 2
 
     def filled(self) -> bool:
-        return self.buffer_start == 0
+        return self.buffer_end == self.BUFFER_SIZE - 1
 
     def random_observations(self, number: int) -> tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor]:
         assert self.buffer_observations_ready()
-        indexes = torch.randint(self.buffer_start, self.BUFFER_SIZE if self.filled() else self.buffer_end, (number,))
-        return (self.observations[indexes],
-                self.observations[(indexes + 1) % self.BUFFER_SIZE],
-                self.rewards[indexes].unsqueeze(1),
-                self.terminations[indexes].unsqueeze(1))
+        indexes = torch.randint(0, self.buffer_end, (number,))
+        states, next_states, rewards, terminations = (self.observations[indexes],
+                                                      self.observations[(indexes + 1) % self.BUFFER_SIZE],
+                                                      self.rewards[indexes].unsqueeze(1),
+                                                      self.terminations[indexes].unsqueeze(1))
+        assert states.shape == next_states.shape == (number, self.nn_input_length)
+        assert rewards.shape == terminations.shape == (number,)
+        return states, next_states, rewards, terminations
