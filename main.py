2e1d8b04dc72cdf263a06e94221b899c3cca2dc1
Joe <30624504+joe-down@users.noreply.github.com>
2024-11-03 22:26:31 +0000

    plotting

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+import itertools
+
 import gymnasium
 import torch.cuda
 from agents.super_agent import SuperAgent
 from runner import Runner
+import matplotlib.pyplot
 
 
-def main(agent_count: int) -> None:
+def main(agent_count: int, plot_interval: int) -> None:
     torch.set_default_device('cuda')
     super_agent = SuperAgent(train_agent_count=agent_count)
     runners = [Runner(env=gymnasium.make("CartPole-v1", render_mode="human"), agent=agent)
                for agent in super_agent.agents]
+
+    figure = matplotlib.pyplot.figure()
+    loss_subplot = figure.add_subplot(1, 1, 1)
+    losses = []
+    figure.show()
+
     try:
-        while True:
+        for iteration in itertools.count():
             for runner in runners:
                 runner.step()
-            super_agent.train()
+            losses.append(super_agent.train())
+            if iteration % plot_interval == 0:
+                loss_subplot.plot(losses)
+                figure.canvas.draw()
+                figure.canvas.flush_events()
     except KeyboardInterrupt:
         for runner in runners:
             runner.close()
@@ -21,4 +34,4 @@ def main(agent_count: int) -> None:
 
 
 if __name__ == '__main__':
-    main(agent_count=2)
+    main(agent_count=8, plot_interval=1000)
