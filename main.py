364c842a1013a91af0527a58868a964d7f44e3bb
Joe <30624504+joe-down@users.noreply.github.com>
2024-11-04 00:02:45 +0000

    progress bar and values

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import torch.cuda
 from agents.super_agent import SuperAgent
 from runner import Runner
 import matplotlib.pyplot
+import tqdm
 
 
 def main(agent_count: int, plot_interval: int) -> None:
     torch.set_default_device('cuda')
     super_agent = SuperAgent(train_agent_count=agent_count)
-    runners = [Runner(env=gymnasium.make("CartPole-v1", render_mode="human"), agent=agent)
+    runners = [Runner(env=gymnasium.make("CartPole-v1", render_mode=None), agent=agent)
                for agent in super_agent.agents]
 
     figure = matplotlib.pyplot.figure()
@@ -19,7 +20,7 @@ def main(agent_count: int, plot_interval: int) -> None:
     figure.show()
 
     try:
-        for iteration in itertools.count():
+        for iteration in tqdm.tqdm(itertools.count()):
             for runner in runners:
                 runner.step()
             losses.append(super_agent.train())
@@ -34,4 +35,4 @@ def main(agent_count: int, plot_interval: int) -> None:
 
 
 if __name__ == '__main__':
-    main(agent_count=8, plot_interval=1000)
+    main(agent_count=2 ** 9, plot_interval=10)
