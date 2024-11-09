c8aa247ee1067abd0a2cb3312a5fd09b55f57714
Joe <30624504+joe-down@users.noreply.github.com>
2024-11-09 12:01:41 +0000

    added non-train mode

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot
 import tqdm
 
 
-def main(agent_count: int,
+def main(train: bool,
+         agent_count: int,
          validation_interval: int,
          validation_repeats: int,
          save_path: str,
@@ -20,7 +21,7 @@ def main(agent_count: int,
          buffer_size: int,
          random_action_probability_decay: float) -> None:
     torch.set_default_device('cuda')
-    super_agent = SuperAgent(train_agent_count=agent_count,
+    super_agent = SuperAgent(train_agent_count=agent_count if train else 0,
                              save_path=save_path,
                              nn_width=nn_width,
                              discount_factor=discount_factor,
@@ -28,6 +29,18 @@ def main(agent_count: int,
                              target_network_update_time=target_network_update_time,
                              buffer_size=buffer_size,
                              random_action_probability_decay=random_action_probability_decay)
+    super_runner = Runner(env=gymnasium.make("CartPole-v1", render_mode=None if train else "human"),
+                          agent=super_agent,
+                          seed=43)
+
+    if not train:
+        try:
+            while True:
+                print(super_runner.run_full())
+        except KeyboardInterrupt:
+            super_runner.close()
+            return
+
     runners = [Runner(env=gymnasium.make("CartPole-v1", render_mode=None), agent=agent, seed=42)
                for agent in super_agent.agents]
     for agent, random_action_minimum in zip(super_agent.agents, numpy.linspace(0, 1, len(super_agent.agents))):
@@ -39,7 +52,6 @@ def main(agent_count: int,
     losses = []
     survival_times_subplot = figure.add_subplot(2, 2, 2)
     survival_times = []
-    super_runner = Runner(env=gymnasium.make("CartPole-v1", render_mode=None), agent=super_agent, seed=43)
     random_probability_subplot = figure.add_subplot(2, 2, 3)
     random_probabilities = []
     figure.show()
@@ -63,17 +75,18 @@ def main(agent_count: int,
     except KeyboardInterrupt:
         for runner in runners:
             runner.close()
-        if agent_count > 0:
-            torch.save(best_state_dict, save_path)
-            print("model saved")
+        super_runner.close()
+        torch.save(best_state_dict, save_path)
+        print("model saved")
 
 
 if __name__ == '__main__':
-    main(agent_count=2 ** 13,
+    main(train=False,
+         agent_count=2 ** 13,
          validation_interval=100,
          validation_repeats=10,
          save_path="model",
-         nn_width=2 ** 8,
+         nn_width=2 ** 9,
          discount_factor=0.9,
          train_batch_size=2 ** 12,
          target_network_update_time=100,
