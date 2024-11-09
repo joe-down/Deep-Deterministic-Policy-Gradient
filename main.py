169b61813b092adc4513ca84e9dfcc3e4b1fc7d6
Joe <30624504+joe-down@users.noreply.github.com>
2024-11-09 12:55:56 +0000

    tidying

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ def main(train: bool,
     super_runner = Runner(env=gymnasium.make(environment, render_mode=None if train else "human"),
                           agent=super_agent,
                           seed=43)
-
     if not train:
         try:
             while True:
@@ -47,44 +46,44 @@ def main(train: bool,
         except KeyboardInterrupt:
             super_runner.close()
             return
+    else:
+        runners = [Runner(env=gymnasium.make(environment, render_mode=None), agent=agent, seed=42)
+                   for agent in super_agent.agents]
+        for agent, random_action_minimum in zip(super_agent.agents, numpy.linspace(0, 1, len(super_agent.agents))):
+            agent.minimum_random_action_probability = random_action_minimum
+        best_state_dict = super_agent.state_dict()
 
-    runners = [Runner(env=gymnasium.make(environment, render_mode=None), agent=agent, seed=42)
-               for agent in super_agent.agents]
-    for agent, random_action_minimum in zip(super_agent.agents, numpy.linspace(0, 1, len(super_agent.agents))):
-        agent.minimum_random_action_probability = random_action_minimum
-    best_state_dict = super_agent.state_dict()
+        figure = matplotlib.pyplot.figure()
+        loss_subplot = figure.add_subplot(2, 2, 1)
+        losses = []
+        survival_times_subplot = figure.add_subplot(2, 2, 2)
+        survival_times = []
+        random_probability_subplot = figure.add_subplot(2, 2, 3)
+        random_probabilities = []
+        figure.show()
 
-    figure = matplotlib.pyplot.figure()
-    loss_subplot = figure.add_subplot(2, 2, 1)
-    losses = []
-    survival_times_subplot = figure.add_subplot(2, 2, 2)
-    survival_times = []
-    random_probability_subplot = figure.add_subplot(2, 2, 3)
-    random_probabilities = []
-    figure.show()
+        try:
+            for iteration in tqdm.tqdm(itertools.count()):
+                for runner in runners:
+                    runner.step()
+                losses.append(super_agent.train())
 
-    try:
-        for iteration in tqdm.tqdm(itertools.count()):
+                if iteration % validation_interval == 0:
+                    loss_subplot.plot(losses)
+                    survival_times.append(numpy.mean([super_runner.run_full() for _ in range(validation_repeats)]))
+                    survival_times_subplot.plot(survival_times)
+                    random_probabilities.append([agent.random_action_probability for agent in super_agent.agents])
+                    random_probability_subplot.plot(random_probabilities)
+                    figure.canvas.draw()
+                    figure.canvas.flush_events()
+                    if len(survival_times) < 2 or survival_times[-1] > max(survival_times[:-1]):
+                        best_state_dict = super_agent.state_dict()
+        except KeyboardInterrupt:
             for runner in runners:
-                runner.step()
-            losses.append(super_agent.train())
-
-            if iteration % validation_interval == 0:
-                loss_subplot.plot(losses)
-                survival_times.append(numpy.mean([super_runner.run_full() for _ in range(validation_repeats)]))
-                survival_times_subplot.plot(survival_times)
-                random_probabilities.append([agent.random_action_probability for agent in super_agent.agents])
-                random_probability_subplot.plot(random_probabilities)
-                figure.canvas.draw()
-                figure.canvas.flush_events()
-                if len(survival_times) < 2 or survival_times[-1] > max(survival_times[:-1]):
-                    best_state_dict = super_agent.state_dict()
-    except KeyboardInterrupt:
-        for runner in runners:
-            runner.close()
-        super_runner.close()
-        torch.save(best_state_dict, save_path)
-        print("model saved")
+                runner.close()
+            super_runner.close()
+            torch.save(best_state_dict, save_path)
+            print("model saved")
 
 
 if __name__ == '__main__':
