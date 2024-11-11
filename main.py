5f7770a9986af4501e12b3d81cba9101566ded71
Joe <30624504+joe-down@users.noreply.github.com>
2024-11-11 16:51:04 +0000

    restructure and add initial bipedal

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ import matplotlib.pyplot
 import tqdm
 
 
-def main(train: bool,
-         agent_count: int,
-         validation_interval: int,
-         validation_repeats: int,
-         save_path: str,
-         nn_width: int,
-         discount_factor: float,
-         train_batch_size: int,
-         target_network_update_time: int,
-         buffer_size: int,
-         random_action_probability_decay: float,
-         observation_length: int,
-         action_length: int,
-         possible_actions: torch.Tensor,
-         environment: str) -> None:
+def run(train: bool,
+        agent_count: int,
+        validation_interval: int,
+        validation_repeats: int,
+        save_path: str,
+        nn_width: int,
+        discount_factor: float,
+        train_batch_size: int,
+        target_network_update_time: int,
+        buffer_size: int,
+        random_action_probability_decay: float,
+        observation_length: int,
+        action_length: int,
+        possible_actions: torch.Tensor,
+        environment: str) -> None:
     torch.set_default_device('cuda')
     super_agent = SuperAgent(train_agent_count=agent_count if train else 0,
                              save_path=save_path,
@@ -86,19 +86,56 @@ def main(train: bool,
             print("model saved")
 
 
+def main(selection: str, train: bool) -> None:
+    match selection:
+        case 'cartpole':
+            agent_count = 2 ** 13
+            validation_interval = 100
+            validation_repeats = 10
+            save_path = "model-cartpole"
+            nn_width = 2 ** 9
+            discount_factor = 0.9
+            train_batch_size = 2 ** 12
+            target_network_update_time = 100
+            buffer_size = 2 ** 15
+            random_action_probability_decay = 1 - 1 / 2 ** 10
+            environment = "CartPole-v1"
+            observation_length = 4
+            action_length = 1
+            possible_actions = torch.tensor([0, 1])
+        case 'bipedal':
+            agent_count = 2 ** 5
+            validation_interval = 1000
+            validation_repeats = 10
+            save_path = "model-bipedal"
+            nn_width = 2 ** 9
+            discount_factor = 0.9
+            train_batch_size = 2 ** 4
+            target_network_update_time = 1000
+            buffer_size = 2 ** 15
+            random_action_probability_decay = 1 - 1 / 2 ** 10
+            environment = "BipedalWalker-v3"
+            observation_length = 24
+            action_length = 4
+            possible_actions = torch.linspace(-1, 1, 2 ** 4)
+        case _:
+            raise NotImplementedError
+    run(train=train,
+        agent_count=agent_count,
+        validation_interval=validation_interval,
+        validation_repeats=validation_repeats,
+        save_path=save_path,
+        nn_width=nn_width,
+        discount_factor=discount_factor,
+        train_batch_size=train_batch_size,
+        target_network_update_time=target_network_update_time,
+        buffer_size=buffer_size,
+        random_action_probability_decay=random_action_probability_decay,
+        environment=environment,
+        observation_length=observation_length,
+        action_length=action_length,
+        possible_actions=possible_actions)
+
+
 if __name__ == '__main__':
-    main(train=False,
-         agent_count=2 ** 13,
-         validation_interval=100,
-         validation_repeats=10,
-         save_path="model",
-         nn_width=2 ** 9,
-         discount_factor=0.9,
-         train_batch_size=2 ** 12,
-         target_network_update_time=100,
-         buffer_size=2 ** 15,
-         random_action_probability_decay=1 - 1 / 2 ** 10,
-         environment="CartPole-v1",
-         observation_length=4,
-         action_length=1,
-         possible_actions=torch.tensor([0, 1]))
+    main(selection='cartpole', train=False)
