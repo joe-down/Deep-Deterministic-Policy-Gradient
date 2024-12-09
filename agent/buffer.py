46cb103ad7266aba5a5599e630de1bf699173dd1
Joe <30624504+joe-down@users.noreply.github.com>
2024-12-09 19:49:48 +0000

    ensure unique random observations

--- agent/buffer.py
+++ agent/buffer.py
@@ -25,6 +25,10 @@ class Buffer:
     def ready(self) -> bool:
         return self.__entry_count >= 2
 
+    @property
+    def __incomplete_index(self) -> int:
+        return (self.__next_index - 1) % self.__buffer_size
+
     @torch.no_grad()
     def push(self,
              observations: torch.Tensor,
@@ -52,16 +56,29 @@ class Buffer:
         assert self.ready
         assert number >= 1
 
-        valid_buffer_indexes = torch.tensor([i for i in range(self.__entry_count)
-                                                  if i != (self.__next_index - 1 % self.__buffer_size)])
-        buffer_indexes = valid_buffer_indexes[torch.randint(0, len(valid_buffer_indexes), (number,))]
-        agent_indexes = torch.randint(0, self.__train_agent_count, (number,))
+        agent_index_starts = (torch.arange(0, self.__train_agent_count) * self.__entry_count).unsqueeze(-1)
+        assert agent_index_starts.shape == (self.__train_agent_count, 1)
+        assert agent_index_starts[-1] == self.__entry_count * (self.__train_agent_count - 1)
+        agent_valid_buffer_indexes = torch.concatenate((
+            torch.arange(0, self.__incomplete_index),
+            torch.arange(self.__incomplete_index + 1, self.__entry_count),
+        )).unsqueeze(0)
+        assert agent_valid_buffer_indexes.shape == (1, self.__entry_count - 1)
+        assert self.__incomplete_index not in agent_valid_buffer_indexes
+        valid_buffer_indexes = (agent_index_starts + agent_valid_buffer_indexes).flatten()
+        assert valid_buffer_indexes.shape == (self.__train_agent_count * (self.__entry_count - 1),)
+        for i in range(self.__train_agent_count):
+            assert i * self.__entry_count + self.__incomplete_index not in valid_buffer_indexes
 
-        observations = self.__observations[buffer_indexes, agent_indexes]
-        actions = self.__actions[buffer_indexes, agent_indexes]
-        rewards = self.__rewards[buffer_indexes, agent_indexes]
-        terminations = self.__terminations[buffer_indexes, agent_indexes]
-        next_observations = self.__observations[(buffer_indexes + 1) % self.__buffer_size, agent_indexes]
+        random_valid_buffer_indexes = valid_buffer_indexes[torch.randperm(valid_buffer_indexes.size(0))[:number]]
+        entry_indexes = random_valid_buffer_indexes // self.__train_agent_count
+        agent_indexes = random_valid_buffer_indexes // self.__entry_count
+
+        observations = self.__observations[entry_indexes, agent_indexes]
+        actions = self.__actions[entry_indexes, agent_indexes]
+        rewards = self.__rewards[entry_indexes, agent_indexes]
+        terminations = self.__terminations[entry_indexes, agent_indexes]
+        next_observations = self.__observations[(entry_indexes + 1) % self.__buffer_size, agent_indexes]
 
         assert observations.shape == (number, self.__observation_length)
         assert actions.shape == (number, self.__action_length)
