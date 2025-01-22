0daa0c081f8dddadcfd3f1a438ee7c03536915ca
Joe <30624504+joe-down@users.noreply.github.com>
2025-01-22 19:03:42 +0000

    actor rework (need to forward to critic correctly

--- actor_critic/actor.py
+++ actor_critic/actor.py
@@ -1,53 +1,70 @@
 import pathlib
-
 import torch
 import typing
-import typing_extensions
-
 from actor_critic.actor_critic_base import ActorCriticBase
+from actor_critic.transformer import Transformer
 
 if typing.TYPE_CHECKING:
     from actor_critic.critic import Critic
 
 
 class Actor(ActorCriticBase):
-    def __init__(self,
-                 load_path: pathlib.Path,
-                 observation_length: int,
-                 action_length: int,
-                 nn_width: int,
-                 nn_depth: int,
-                 ) -> None:
+    def __init__(
+            self,
+            load_path: pathlib.Path,
+            observation_length: int,
+            action_length: int,
+            history_size: int,
+            n_head: int,
+    ) -> None:
+        self.__observation_length = observation_length
         self.__action_length = action_length
-        neural_network = torch.nn.Sequential(
-            torch.nn.Linear(observation_length, nn_width),
-            torch.nn.ReLU(),
+        self.__history_size = history_size
+        model = Transformer(
+            in_features=observation_length,
+            out_features=action_length,
+            history_size=history_size,
+            n_head=n_head,
         )
-        for _ in range(nn_depth):
-            neural_network.append(torch.nn.Linear(nn_width, nn_width))
-            neural_network.append(torch.nn.ReLU())
-        neural_network.append(torch.nn.Linear(nn_width, action_length))
-        neural_network.append(torch.nn.Sigmoid())
-        super().__init__(load_path=load_path / "action", neural_network=neural_network)
-        self.__optimiser = torch.optim.AdamW(params=self._parameters)
+        super().__init__(
+            load_path=load_path / "action",
+            model=model,
+            model_input_shape=(history_size, observation_length),
+            model_output_shape=(action_length,),
+        )
+        self.__optimiser = torch.optim.AdamW(params=self._model_parameters)
 
-    @typing_extensions.override
-    @property
-    def _nn_output_shape(self):
-        return (self.__action_length,)
-
-    def update(self,
-               observations: torch.Tensor,
-               target_update_proportion: float,
-               critic: "Critic",
-               update_target_network: bool,
-               ) -> float:
-        best_actions = self.forward_network(observations)
-        best_observation_actions = torch.concatenate((observations, best_actions), dim=1)
+    def update(
+            self,
+            src: torch.Tensor,
+            tgt: torch.Tensor,
+            src_key_padding_mask: torch.Tensor,
+            tgt_key_padding_mask: torch.Tensor,
+            target_update_proportion: float,
+            critic: "Critic",
+            update_target_network: bool,
+    ) -> float:
+        best_actions = self.forward_model(
+            src=src,
+            tgt=tgt,
+            src_key_padding_mask=src_key_padding_mask,
+            tgt_key_padding_mask=tgt_key_padding_mask,
+        )
+        assert best_actions.shape == src.shape[:-2] + (self.__action_length,)
+        history_dimension_best_actions = best_actions.unsqueeze(dim=-2)
+        assert history_dimension_best_actions.shape == src.shape[:-2] + (1, self.__action_length)
+        history_repeated_best_actions = history_dimension_best_actions.repeat_interleave(
+            repeats=self.__history_size,
+            dim=-2,
+        )
+        assert history_repeated_best_actions.shape == src.shape[:-1] + (self.__action_length,)
+        best_src_actions = torch.concatenate((src, history_repeated_best_actions), dim=-1)
+        assert best_src_actions.shape == src.shape[:-1] + (self.__observation_length + self.__action_length,)
         self.__optimiser.zero_grad()
-        loss = (-critic.forward_network(best_observation_actions)).mean()
+        loss = (-critic.forward_network()).mean()  # TODO critic forward
+        assert loss.shape == ()
         loss.backward()
         self.__optimiser.step()
         if update_target_network:
-            self._update_target_network(target_update_proportion=target_update_proportion)
+            self._update_target_model(target_update_proportion=target_update_proportion)
         return -loss
