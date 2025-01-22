24b4ea5a83bd1bf5c1fb791c6d8fe0ff1f7e70eb
Joe <30624504+joe-down@users.noreply.github.com>
2025-01-22 20:21:26 +0000

    clearer parameter names

--- actor_critic/actor.py
+++ actor_critic/actor.py
@@ -36,35 +36,36 @@ class Actor(ActorCriticBase):
 
     def update(
             self,
-            src: torch.Tensor,
+            observations: torch.Tensor,
             tgt: torch.Tensor,
-            src_key_padding_mask: torch.Tensor,
+            observations_key_padding_mask: torch.Tensor,
             tgt_key_padding_mask: torch.Tensor,
-            target_update_proportion: float,
-            critic: "Critic",
+            target_model_update_proportion: float,
             update_target_network: bool,
+            critic: "Critic",
     ) -> float:
         best_actions = self.forward_model(
-            src=src,
+            src=observations,
             tgt=tgt,
-            src_key_padding_mask=src_key_padding_mask,
+            src_key_padding_mask=observations_key_padding_mask,
             tgt_key_padding_mask=tgt_key_padding_mask,
         )
-        assert best_actions.shape == src.shape[:-2] + (self.__action_length,)
+        assert best_actions.shape == observations.shape[:-2] + (self.__action_length,)
         history_dimension_best_actions = best_actions.unsqueeze(dim=-2)
-        assert history_dimension_best_actions.shape == src.shape[:-2] + (1, self.__action_length)
+        assert history_dimension_best_actions.shape == observations.shape[:-2] + (1, self.__action_length)
         history_repeated_best_actions = history_dimension_best_actions.repeat_interleave(
             repeats=self.__history_size,
             dim=-2,
         )
-        assert history_repeated_best_actions.shape == src.shape[:-1] + (self.__action_length,)
-        best_src_actions = torch.concatenate((src, history_repeated_best_actions), dim=-1)
-        assert best_src_actions.shape == src.shape[:-1] + (self.__observation_length + self.__action_length,)
+        assert history_repeated_best_actions.shape == observations.shape[:-1] + (self.__action_length,)
+        best_observation_actions = torch.concatenate((observations, history_repeated_best_actions), dim=-1)
+        assert best_observation_actions.shape == observations.shape[:-1] + (
+        self.__observation_length + self.__action_length,)
         self.__optimiser.zero_grad()
         loss = (-critic.forward_network()).mean()  # TODO critic forward
         assert loss.shape == ()
         loss.backward()
         self.__optimiser.step()
         if update_target_network:
-            self._update_target_model(target_update_proportion=target_update_proportion)
+            self._update_target_model(target_update_proportion=target_model_update_proportion)
         return -loss
