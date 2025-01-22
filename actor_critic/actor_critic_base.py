5911bdcdd651b70ab4ee7120ba4c137b06ccbcc6
Joe <30624504+joe-down@users.noreply.github.com>
2025-01-22 17:48:45 +0000

    actor_critic_base.py rewrite

--- actor_critic/actor_critic_base.py
+++ actor_critic/actor_critic_base.py
@@ -24,13 +24,12 @@ class ActorCriticBase(abc.ABC):
             self.__model.load_state_dict(torch.load(load_path))
             print("model loaded")
         except FileNotFoundError:
-            self.__model.apply(self.__model_initialisation)
+            self.__model.apply(self.__initialise_model)
             print("model initialised")
         self.__target_model = copy.deepcopy(self.__model)
-        self._update_target_model(target_update_proportion=1)
 
     @staticmethod
-    def __model_initialisation(module: torch.nn.Module) -> None:
+    def __initialise_model(module: torch.nn.Module) -> None:
         if isinstance(module, torch.nn.Linear):
             torch.nn.init.xavier_uniform_(module.weight)
 
@@ -42,49 +41,69 @@ class ActorCriticBase(abc.ABC):
     def model_state_dict(self) -> dict[str, typing.Any]:
         return self.__model.state_dict()
 
-    @abc.abstractmethod
-    def _src_key_padding_mask(self, src: torch.Tensor, step_number: int) -> torch.Tensor:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def _tgt_key_padding_mask(self, tgt: torch.Tensor, step_number: int) -> torch.Tensor:
-        raise NotImplementedError
-
-    def __forward_model_base(self,
-                             observations: torch.Tensor,
-                             tgt: torch.Tensor,
-                             step_number: int,
-                             model: torch.nn.Module
-                             ) -> torch.Tensor:
-        assert observations.shape[-len(self.__model_input_shape):] == self.__model_input_shape
-        assert tgt.shape == observations.shape
+    def __forward_model_base(
+            self,
+            src: torch.Tensor,
+            tgt: torch.Tensor,
+            src_mask: torch.Tensor,
+            tgt_mask: torch.Tensor,
+            src_key_padding_mask: torch.Tensor,
+            tgt_key_padding_mask: torch.Tensor,
+            model: torch.nn.Module,
+    ) -> torch.Tensor:
+        assert src.shape[-len(self.__model_input_shape):] == self.__model_input_shape
         result = model.forward(
-            src=observations,
+            src=src,
             tgt=tgt,
-            src_key_padding_mask=self._src_key_padding_mask(src=observations, step_number=step_number),
-            tgt_key_padding_mask=self._tgt_key_padding_mask(tgt=observations, step_number=step_number),
+            src_mask=src_mask,
+            tgt_mask=tgt_mask,
+            src_key_padding_mask=src_key_padding_mask,
+            tgt_key_padding_mask=tgt_key_padding_mask,
         )
-        assert result.shape == observations.shape[:-len(self.__model_input_shape)] + self.__model_output_shape
+        assert result.shape == src.shape[:-len(self.__model_input_shape)] + self.__model_output_shape
         return result
 
-    def forward_model(self, observations: torch.Tensor, tgt: torch.Tensor, step_number: int) -> torch.Tensor:
+    def forward_model(
+            self,
+            src: torch.Tensor,
+            tgt: torch.Tensor,
+            src_mask: torch.Tensor,
+            tgt_mask: torch.Tensor,
+            src_key_padding_mask: torch.Tensor,
+            tgt_key_padding_mask: torch.Tensor,
+    ) -> torch.Tensor:
         return self.__forward_model_base(
-            observations=observations,
+            src=src,
             tgt=tgt,
+            src_mask=src_mask,
+            tgt_mask=tgt_mask,
+            src_key_padding_mask=src_key_padding_mask,
+            tgt_key_padding_mask=tgt_key_padding_mask,
             model=self.__model,
-            step_number=step_number,
         )
 
-    def forward_target_model(self, observations: torch.Tensor, tgt: torch.Tensor, step_number: int) -> torch.Tensor:
+    def forward_target_model(
+            self,
+            src: torch.Tensor,
+            tgt: torch.Tensor,
+            src_mask: torch.Tensor,
+            tgt_mask: torch.Tensor,
+            src_key_padding_mask: torch.Tensor,
+            tgt_key_padding_mask: torch.Tensor,
+    ) -> torch.Tensor:
         return self.__forward_model_base(
-            observations=observations,
+            src=src,
             tgt=tgt,
+            src_mask=src_mask,
+            tgt_mask=tgt_mask,
+            src_key_padding_mask=src_key_padding_mask,
+            tgt_key_padding_mask=tgt_key_padding_mask,
             model=self.__target_model,
-            step_number=step_number,
+
         )
 
     def _update_target_model(self, target_update_proportion: float) -> None:
         assert 0 <= target_update_proportion <= 1
         for parameter, target_parameter in zip(self.__model.parameters(), self.__target_model.parameters()):
-            target_parameter.data = ((1 - target_update_proportion) * target_parameter.data
-                                     + target_update_proportion * parameter.data)
+            target_parameter.data \
+                = ((1 - target_update_proportion) * target_parameter.data + target_update_proportion * parameter.data)
