b818f18ee6b5b60247076fbd90a2c5a508519c92
Joe <30624504+joe-down@users.noreply.github.com>
2025-02-01 14:25:08 +0000

    critic rework

--- actor_critic/critic.py
+++ actor_critic/critic.py
@@ -18,8 +18,6 @@ class Critic:
             embedding_dim: int,
             n_head: int,
     ) -> None:
-        self.__observation_length = observation_length
-        self.__action_length = action_length
         self.__history_size = history_size
         self.__sub_critic_count = sub_critic_count
         self.__sub_critics = [SubCritic(
@@ -31,156 +29,48 @@ class Critic:
             n_head=n_head,
         ) for i in range(sub_critic_count)]
         self.__loss_function = torch.nn.HuberLoss()
-        self.__main_network_a = True
 
     @property
     def model_state_dicts(self) -> tuple[dict[str, typing.Any], ...]:
         return tuple(sub_critic.model_state_dict for sub_critic in self.__sub_critics)
 
-    @staticmethod
-    def __forward_model_base(q_rewards: torch.Tensor) -> torch.Tensor:
-        least_reward_values, _ = q_rewards.min(dim=0)
-        assert least_reward_values.shape == q_rewards.shape[1:]
+    def forward_model(
+            self,
+            observation_actions: torch.Tensor,
+            observation_actions_sequence_length: torch.Tensor,
+    ) -> torch.Tensor:
+        q_rewards = torch.concatenate([sub_critic.forward_model(
+            observation_actions=observation_actions,
+            observation_actions_sequence_length=observation_actions_sequence_length,
+        ) for sub_critic in self.__sub_critics], dim=-1)
+        assert q_rewards.shape == observation_actions.shape[:-2] + (self.__sub_critic_count,)
+        least_reward_values, _ = q_rewards.min(dim=-1)
+        assert least_reward_values.shape == q_rewards.shape[:-1]
         return least_reward_values
-
-    def forward_model_a(
-            self,
-            observation_actions: torch.Tensor,
-            observation_actions_sequence_length: torch.Tensor,
-    ) -> torch.Tensor:
-        return self.__forward_model_base(q_rewards=torch.stack([sub_critic.forward_model_a(
-            observation_actions=observation_actions,
-            observation_actions_sequence_length=observation_actions_sequence_length,
-        ) for sub_critic in self.__sub_critics]))
-
-    def forward_model_b(
-            self,
-            observation_actions: torch.Tensor,
-            observation_actions_sequence_length: torch.Tensor,
-    ) -> torch.Tensor:
-        return self.__forward_model_base(q_rewards=torch.stack([sub_critic.forward_model_b(
-            observation_actions=observation_actions,
-            observation_actions_sequence_length=observation_actions_sequence_length,
-        ) for sub_critic in self.__sub_critics]))
-
-    def observation_actions(self, observations: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
-        assert observations.ndim >= 2
-        assert observations.shape[-2:] == (self.__history_size, self.__observation_length)
-        assert actions.shape == observations.shape[:-2] + (self.__history_size, self.__action_length)
-        observation_actions = torch.concatenate((observations, actions), dim=-1)
-        assert (observation_actions.shape
-                == observations.shape[:-2] + (self.__history_size, self.__observation_length + self.__action_length))
-        assert torch.all(observation_actions[..., :self.__observation_length] == observations)
-        assert torch.all(observation_actions[..., self.__observation_length:] == actions)
-        return observation_actions
 
     def update(
             self,
             actor: "Actor",
             observations: torch.Tensor,
             actions: torch.Tensor,
-            previous_observations: torch.Tensor,
-            previous_actions: torch.Tensor,
             observations_sequence_length: torch.Tensor,
-            previous_observations_sequence_length: torch.Tensor,
             next_observations: torch.Tensor,
             next_observations_sequence_length: torch.Tensor,
             immediate_rewards: torch.Tensor,
             terminations: torch.Tensor,
             discount_factor: float,
     ) -> float:
-        assert observations.ndim >= 2
-        assert observations.shape[-2:] == (self.__history_size, self.__observation_length)
-        assert actions.shape == observations.shape[:-2] + (self.__history_size, self.__action_length)
-        assert previous_observations.shape == observations.shape
-        assert torch.all(previous_observations[..., 1:, :] == observations[..., :-1, :])
-        assert previous_actions.shape == actions.shape
-        assert torch.all(previous_actions[..., 1:, :] == actions[..., :-1, :])
-        assert observations_sequence_length.shape == observations.shape[:-2]
-        assert previous_observations_sequence_length.shape == previous_observations.shape[:-2]
-        assert next_observations.shape == observations.shape
-        assert torch.all(next_observations[..., :-1, :] == observations[..., 1:, :])
-        assert next_observations_sequence_length.shape == observations_sequence_length.shape
-        assert immediate_rewards.shape == observations.shape[:-2]
-        assert terminations.shape == observations.shape[:-2]
-        assert 0 <= discount_factor <= 1
-        observation_actions = self.observation_actions(observations=observations, actions=actions)
-        previous_observation_actions = self.observation_actions(
-            observations=previous_observations,
-            actions=previous_actions,
-        )
-        assert torch.all(previous_observation_actions[..., 1:, :] == observation_actions[..., :-1, :])
-
-        squeezed_best_next_action = actor.forward_target_model(
-            observations=next_observations,
-            previous_actions=actions[..., 1:, :],
-            observations_sequence_length=next_observations_sequence_length,
-        )
-        assert squeezed_best_next_action.shape == next_observations.shape[:-2] + (self.__action_length,)
-        best_next_action = squeezed_best_next_action.unsqueeze(dim=-2)
-        assert best_next_action.shape == next_observations.shape[:-2] + (1, self.__action_length,)
-        best_next_actions = torch.concatenate(tensors=(actions[..., 1:, :], best_next_action), dim=-2)
-        assert best_next_actions.shape == actions.shape
-        best_next_observation_actions = self.observation_actions(
-            observations=next_observations,
-            actions=best_next_actions,
-        )
-        assert best_next_observation_actions.shape == observation_actions.shape
-        assert torch.all(best_next_observation_actions[..., :-1, :self.__observation_length]
-                         == observations[..., 1:, :])
-        assert torch.all(best_next_observation_actions[..., :-1, self.__observation_length:] == actions[..., 1:, :])
-        assert torch.all(best_next_observation_actions[..., -1:, :self.__observation_length]
-                         == next_observations[..., -1:, :])
-        assert torch.all(best_next_observation_actions[..., -1:, self.__observation_length:] == best_next_action)
-
-        worst_best_next_observation_actions_q = self.forward_model_a(
-            observation_actions=best_next_observation_actions,
-            observation_actions_sequence_length=next_observations_sequence_length,
-        ) if not self.__main_network_a else self.forward_model_b(
-            observation_actions=best_next_observation_actions,
-            observation_actions_sequence_length=next_observations_sequence_length,
-        )
-        assert worst_best_next_observation_actions_q.shape == observations.shape[:-2]
-        q_targets = (immediate_rewards
-                     + discount_factor * (1 - terminations) * worst_best_next_observation_actions_q).unsqueeze(dim=-1)
-        assert q_targets.shape == observations.shape[:-2] + (1,)
-        sub_critic_observation_count = observation_actions.size(dim=-3) // self.__sub_critic_count
-        assert sub_critic_observation_count > 0
-        assert sub_critic_observation_count * self.__sub_critic_count <= observation_actions.size(dim=-3)
         loss = torch.tensor(data=[sub_critic.update(
-            observation_actions
-            =observation_actions[
-             ...,
-             sub_critic_number * sub_critic_observation_count:(sub_critic_number + 1) * sub_critic_observation_count,
-             :,
-             :,
-             ].detach(),
-            previous_observation_actions
-            =previous_observation_actions[
-             ...,
-             sub_critic_number * sub_critic_observation_count:(sub_critic_number + 1) * sub_critic_observation_count,
-             :,
-             :,
-             ].detach(),
-            observation_actions_sequence_length
-            =observations_sequence_length[
-             ...,
-             sub_critic_number * sub_critic_observation_count:(sub_critic_number + 1) * sub_critic_observation_count,
-             ].detach(),
-            previous_observation_actions_sequence_length
-            =previous_observations_sequence_length[
-             ...,
-             sub_critic_number * sub_critic_observation_count:(sub_critic_number + 1) * sub_critic_observation_count,
-             ].detach(),
-            q_targets
-            =q_targets[
-             ...,
-             sub_critic_number * sub_critic_observation_count:(sub_critic_number + 1) * sub_critic_observation_count,
-             :,
-             ].detach(),
+            observation=observations.detach(),
+            action=actions.detach(),
+            next_observation=next_observations.detach(),
+            observation_sequence_length=observations_sequence_length.detach(),
+            next_observation_sequence_length=next_observations_sequence_length.detach(),
+            immediate_reward=immediate_rewards.unsqueeze(-1).detach(),
+            termination=terminations.unsqueeze(-1).detach(),
+            discount_factor=discount_factor,
             loss_function=self.__loss_function,
-            main_network_a=self.__main_network_a,
-        ) for sub_critic_number, sub_critic in enumerate(self.__sub_critics)]).mean()
+            actor=actor,
+        ) for sub_critic in self.__sub_critics]).mean()
         assert loss.shape == ()
-        self.__main_network_a = not self.__main_network_a
         return loss.item()
--- actor_critic/sub_critic.py
+++ actor_critic/sub_critic.py
@@ -1,5 +1,7 @@
 import pathlib
 import torch
+
+from actor_critic.actor import Actor
 from actor_critic.actor_critic_base import ActorCriticBase
 from actor_critic.model import Model
 
@@ -16,6 +18,8 @@ class SubCritic(ActorCriticBase):
     ) -> None:
         assert observation_length > 0
         assert action_length > 0
+        self.__observation_length = observation_length
+        self.__action_length = action_length
         super().__init__(
             load_path=load_path,
             model=Model(
@@ -30,72 +34,92 @@ class SubCritic(ActorCriticBase):
             history_size=history_size,
         )
         self.__optimiser = torch.optim.AdamW(params=self._model_parameters)
+        self.__main_network_a = True
 
-    def __forward_model_postprocess(self, observation_actions: torch.Tensor, qs: torch.Tensor) -> torch.Tensor:
-        assert qs.shape == observation_actions.shape[:-2] + (self._history_size, self._output_features)
-        assert self._output_features == 1
-        q = qs[..., -1, :].squeeze(dim=-1)
-        assert q.shape == observation_actions.shape[:-2]
+    def __forward_model_postprocess(self, qs: torch.Tensor) -> torch.Tensor:
+        assert qs.shape[-2:] + (self._history_size, self._output_features,)
+        q = qs[..., -1, :]
+        assert q.shape == qs.shape[:-2] + qs.shape[-1:]
         return q
 
-    def forward_model_a(
+    def forward_model(
             self,
             observation_actions: torch.Tensor,
             observation_actions_sequence_length: torch.Tensor,
     ) -> torch.Tensor:
-        return self.__forward_model_postprocess(
-            observation_actions=observation_actions,
-            qs=self._forward_model_no_tgt(
-                src=observation_actions,
-                src_sequence_length=observation_actions_sequence_length,
-            ),
-        )
+        return self.__forward_model_postprocess(qs=self._forward_model_no_tgt(
+            src=observation_actions,
+            src_sequence_length=observation_actions_sequence_length,
+        ))
 
-    def forward_model_b(
+    def __forward_model_b(
             self,
             observation_actions: torch.Tensor,
             observation_actions_sequence_length: torch.Tensor,
     ) -> torch.Tensor:
-        return self.__forward_model_postprocess(
-            observation_actions=observation_actions,
-            qs=self._forward_target_model_no_tgt(
-                src=observation_actions,
-                src_sequence_length=observation_actions_sequence_length,
-            ),
-        )
+        return self.__forward_model_postprocess(qs=self._forward_target_model_no_tgt(
+            src=observation_actions,
+            src_sequence_length=observation_actions_sequence_length,
+        ))
+
+    def __observation_action(self, observation: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
+        assert observation.ndim >= 2
+        assert observation.shape[-2:] == (self._history_size, self.__observation_length)
+        assert action.shape == observation.shape[:-1] + (self.__action_length,)
+        observation_actions = torch.concatenate((observation, action), dim=-1)
+        assert observation_actions.shape == observation.shape[:-1] + (self._input_features,)
+        assert torch.all(observation_actions[..., :self.__observation_length] == observation)
+        assert torch.all(observation_actions[..., self.__observation_length:] == action)
+        return observation_actions
 
     def update(
             self,
-            observation_actions: torch.Tensor,
-            previous_observation_actions: torch.Tensor,
-            observation_actions_sequence_length: torch.Tensor,
-            previous_observation_actions_sequence_length: torch.Tensor,
-            q_targets: torch.Tensor,
+            observation: torch.Tensor,
+            action: torch.Tensor,
+            next_observation: torch.Tensor,
+            observation_sequence_length: torch.Tensor,
+            next_observation_sequence_length: torch.Tensor,
+            immediate_reward: torch.Tensor,
+            termination: torch.Tensor,
+            discount_factor: float,
             loss_function: torch.nn.Module,
-            main_network_a: bool,
+            actor: Actor,
     ) -> float:
-        assert observation_actions.ndim >= 2
-        assert q_targets.shape == observation_actions.shape[:-2] + (self._output_features,)
-        previous_qs = self._forward_target_model_no_tgt(
-            src=previous_observation_actions.detach(),
-            src_sequence_length=previous_observation_actions_sequence_length.detach(),
+        assert immediate_reward.shape == observation.shape[:-2] + (1,)
+        assert termination.shape == observation.shape[:-2] + (1,)
+        assert 0 <= discount_factor <= 1
+        observation_action = self.__observation_action(observation=observation, action=action)
+        no_history_next_action = actor.forward_model(
+            observations=next_observation,
+            previous_actions=action[..., 1:, :],
+            observations_sequence_length=next_observation_sequence_length,
+        ).unsqueeze(dim=-2)
+        assert no_history_next_action.shape == action.shape[:-2] + (1,) + action.shape[-1:]
+        next_action = torch.concatenate(tensors=(action[..., 1:, :], no_history_next_action), dim=-2)
+        assert next_action.shape == action.shape
+        assert torch.all(next_action[..., :-1, :] == action[..., 1:, :])
+        assert torch.all(next_action[..., -1:, :] == no_history_next_action)
+        next_observation_action = self.__observation_action(observation=next_observation, action=next_action)
+        next_qs = self.forward_model(
+            observation_actions=next_observation_action,
+            observation_actions_sequence_length=next_observation_sequence_length,
+        ) if not self.__main_network_a else self.__forward_model_b(
+            observation_actions=next_observation_action,
+            observation_actions_sequence_length=next_observation_sequence_length,
         )
-        assert previous_qs.shape == observation_actions.shape[:-2] + (self._history_size, self._output_features)
-        loss_target = torch.concatenate(tensors=(previous_qs[..., 1:, :], q_targets.unsqueeze(dim=-2)), dim=-2)
-        assert loss_target.shape == observation_actions.shape[:-2] + (self._history_size, self._output_features)
-        prediction = self._forward_model(
-            src=observation_actions.detach(),
-            tgt=previous_qs[..., 1:, :].detach(),
-            src_sequence_length=observation_actions_sequence_length.detach(),
-        ) if main_network_a else self._forward_target_model(
-            src=observation_actions.detach(),
-            tgt=previous_qs[..., 1:, :].detach(),
-            src_sequence_length=observation_actions_sequence_length.detach(),
+        q_targets = immediate_reward + discount_factor * (1 - termination) * next_qs
+        assert q_targets.shape == observation_action.shape[:-2] + (1,)
+        prediction = self.forward_model(
+            observation_actions=observation_action,
+            observation_actions_sequence_length=observation_sequence_length,
+        ) if self.__main_network_a else self.__forward_model_b(
+            observation_actions=observation_action,
+            observation_actions_sequence_length=observation_sequence_length,
         )
-        assert prediction.shape == observation_actions.shape[:-2] + (self._history_size, self._output_features)
         self.__optimiser.zero_grad()
-        loss = loss_function.forward(input=prediction, target=loss_target.detach())
+        loss = loss_function.forward(input=prediction, target=q_targets)
         assert loss.shape == ()
         loss.backward()
         self.__optimiser.step()
-        return loss.item()
+        self.__main_network_a = not self.__main_network_a
+        return loss
