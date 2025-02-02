3d1a6a67693735db1011d9c5450f70b617937553
Joe <30624504+joe-down@users.noreply.github.com>
2025-02-02 18:35:00 +0000

    critic tweaks

--- actor_critic/sub_critic.py
+++ actor_critic/sub_critic.py
@@ -34,7 +34,6 @@ class SubCritic(ActorCriticBase):
         )
         self.__optimiser_a = torch.optim.AdamW(params=self._model_a_parameters)
         self.__optimiser_b = torch.optim.AdamW(params=self._model_b_parameters)
-        self.__main_network_a = True
 
     def __observation_action(self, observation: torch.Tensor, action: torch.Tensor) -> torch.Tensor:
         assert observation.ndim >= 2
@@ -63,41 +62,38 @@ class SubCritic(ActorCriticBase):
             loss_function: torch.nn.Module,
             actor: Actor,
     ) -> float:
-        assert immediate_reward.shape == observation.shape[:-2] + (self._output_features,)
-        assert termination.shape == observation.shape[:-2] + (self._output_features,)
-        assert 0 <= discount_factor <= 1
-
-        no_history_next_action = actor.forward(observation=next_observation).unsqueeze(dim=-2)#TODO
+        no_history_next_action = actor.forward(observation=next_observation).unsqueeze(dim=-2)  # TODO
         assert no_history_next_action.shape == action.shape[:-2] + (1,) + action.shape[-1:]
         next_action = torch.concatenate(tensors=(action[..., 1:, :], no_history_next_action), dim=-2)
         assert next_action.shape == action.shape
         assert torch.all(next_action[..., :-1, :] == action[..., 1:, :])
         assert torch.all(next_action[..., -1:, :] == no_history_next_action)
-        next_observation_action \
-            = self.__observation_action(observation=next_observation, action=next_action).flatten(-2, -1)  # TODO
+        next_observation_action = self.__observation_action(observation=next_observation, action=next_action)
 
-        next_qs = self._forward_model_a(next_observation_action) if not self.__main_network_a \
-            else self._forward_model_b(next_observation_action)
-        assert next_qs.shape == observation.shape[:-2] + (self._output_features,)
+        next_qs_a = self._forward_model_a(next_observation_action.flatten(-2, -1).detach()).unsqueeze(-1)  # TODO
+        assert next_qs_a.shape == next_observation_action.shape[:-2] + (self._output_features, 1)
+        next_qs_b = self._forward_model_b(next_observation_action.flatten(-2, -1).detach()).unsqueeze(-1)  # TODO
+        assert next_qs_b.shape == next_observation_action.shape[:-2] + (self._output_features, 1)
+        next_qs, minimum_q_indices = torch.concatenate((next_qs_a, next_qs_b), dim=-1).min(dim=-1)
+        assert next_qs.shape == next_observation_action.shape[:-2] + (self._output_features,)
         q_targets = immediate_reward + discount_factor * (1 - termination) * next_qs
-        assert q_targets.shape == observation.shape[:-2] + (self._output_features,)
 
         observation_action = self.__observation_action(observation=observation, action=action).flatten(-2, -1)  # TODO
-        prediction = self._forward_model_a(observation_action) if self.__main_network_a \
-            else self._forward_model_b(observation_action)
-        assert prediction.shape == observation.shape[:-2] + (self._output_features,)
+        prediction_a = self._forward_model_a(observation_action.detach())
+        assert prediction_a.shape == observation.shape[:-2] + (self._output_features,)
+        prediction_b = self._forward_model_b(observation_action.detach())
+        assert prediction_b.shape == prediction_a.shape
 
-        if self.__main_network_a:
-            self.__optimiser_a.zero_grad()
-        else:
-            self.__optimiser_b.zero_grad()
-        loss = loss_function.forward(input=prediction, target=q_targets)
-        assert loss.shape == ()
-        loss.backward()
-        if self.__main_network_a:
-            self.__optimiser_a.step()
-        else:
-            self.__optimiser_b.step()
+        self.__optimiser_a.zero_grad()
+        loss_a = loss_function.forward(input=prediction_a, target=q_targets.detach())
+        assert loss_a.shape == ()
+        loss_a.backward()
+        self.__optimiser_a.step()
 
-        self.__main_network_a = not self.__main_network_a
-        return loss
+        self.__optimiser_b.zero_grad()
+        loss_b = loss_function.forward(input=prediction_b, target=q_targets.detach())
+        assert loss_b.shape == ()
+        loss_b.backward()
+        self.__optimiser_b.step()
+
+        return loss_a + loss_b
